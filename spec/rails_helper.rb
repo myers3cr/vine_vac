9a66d73ca599b190045b5911f5aaf0693119a857
C.R. Myers <myers3.cr@gmail.com>
2016-06-02 15:29:01 -0400

    Basic app setup with gems, rspec and pg

---
+++ spec/rails_helper.rb
@@ -0,0 +1,43 @@
+  require 'spec_helper'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+  require 'capybara/rspec'
+  require 'simple_bdd'
+  require 'shoulda/matchers'
+  
+  Capybara.javascript_driver = :webkit
+
+  ActiveRecord::Migration.maintain_test_schema!
+
+  RSpec.configure do |config|
+    config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+    config.use_transactional_fixtures = true
+    
+    config.before(:suite) do
+      DatabaseCleaner.strategy = :truncation
+      DatabaseCleaner.clean_with(:truncation)
+    end 
+
+    config.before(:each) do
+      DatabaseCleaner.start
+    end
+
+    config.after(:each) do
+      DatabaseCleaner.clean
+    end
+
+    config.include SimpleBdd, type: :feature
+    
+    Shoulda::Matchers.configure do |config|
+      config.integrate do |with|
+        with.test_framework :rspec
+        with.library :rails
+      end
+    end
+    
+    config.infer_spec_type_from_file_location!
+
+    config.filter_rails_from_backtrace!   
+
+  end

da356979a5be308d14e5fd267fb3d9cbf3a39a93
C.R. Myers <myers3.cr@gmail.com>
2016-06-02 16:14:41 -0400

    Added basic home controller

---
+++ spec/features/home_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+feature "visiting the homepage" do
+  
+  scenario "the visitor sees welcome text" do
+    visit root_path
+    expect(page).to have_text("Welcome to Vineyard Vacations")
+  end
+
+end

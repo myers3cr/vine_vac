d1ce9197c1eab6b5f9fee279a46de9c39b95ada7
C.R. Myers <myers3.cr@gmail.com>
2016-06-06 14:14:12 -0400

    Added home page tests for links & buttons Added Bootstrap 'Sign in' and 'Sign up' forms

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,10 +1,26 @@
 require 'rails_helper'
 
 feature "visiting the homepage" do
+
+  before(:each) do
+    visit root_path
+  end
   
   scenario "the visitor sees welcome text" do
     visit root_path
     expect(page).to have_text("Welcome to Vineyard Vacations")
   end
+  scenario "the visitor sees 'join us' button" do
+    visit root_path
+    expect(page).to have_button("Join Us!")
+  end
+  scenario "the visitor sees 'sign up' link" do
+    visit root_path
+    expect(page).to have_link("Sign up")
+  end
+  scenario "the visitor sees 'sign in' link" do
+    visit root_path
+    expect(page).to have_link("Sign in")
+  end 
 
 end

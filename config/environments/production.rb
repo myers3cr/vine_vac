838a172a3a207190fc078b7e38b169666280ae4f
C.R. Myers <myers3.cr@gmail.com>
2016-07-05 16:47:31 -0400

    Updated email provider to SendGrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,10 +16,11 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
+    address:              'smtp.sendgrid.com',
     port:                 587,
-    user_name:            ENV['GMAIL_USERNAME'],
-    password:             ENV['GMAIL_PASSWORD'],
+    domain:               'crmyers3.com'
+    user_name:            ENV['SENDGRID_USERNAME'],
+    password:             ENV['SENDGRID_PASSWORD'],
     authentication:       :plain,
     enable_starttls_auto: true
   }

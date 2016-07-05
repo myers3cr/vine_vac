3cb575658e5c7aa88d23298f70ba92f41dc7e66d
C.R. Myers <myers3.cr@gmail.com>
2016-07-05 15:17:28 -0400

    Added production mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,6 +12,18 @@ Rails.application.configure do
     }
   }
 
+  # Mailer settings
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            ENV['GMAIL_USERNAME'],
+    password:             ENV['GMAIL_PASSWORD'],
+    authentication:       :plain,
+    enable_starttls_auto: true
+  }
+
 # Code is not reloaded between requests.
   config.cache_classes = true
 

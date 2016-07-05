45215c95b2b6a2e1a8923e3b98da4c96ac0e81a4
C.R. Myers <myers3.cr@gmail.com>
2016-07-05 17:02:05 -0400

    Fix SendGrid address in production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,9 +16,9 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.smtp_settings = {
-    address:              'smtp.sendgrid.com',
+    address:              'smtp.sendgrid.net',
     port:                 587,
-    domain:               'crmyers3.com',
+    domain:               'sheltered-basin-86048.herokuapp.com',
     user_name:            ENV['SENDGRID_USERNAME'],
     password:             ENV['SENDGRID_PASSWORD'],
     authentication:       :plain,

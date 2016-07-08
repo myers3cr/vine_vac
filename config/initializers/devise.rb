5911acbd4be4ffe30dea5b9b92a37871cf4b1feb
C.R. Myers <myers3.cr@gmail.com>
2016-07-07 23:21:52 -0400

    tried putting secret key in devise initializer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,7 +6,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` on Rails 4+ applications as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = '5c81d0b42866aef97ed234489a52feb3f26cd810814f86c8c77a53bec5676fdf575edffa7351cc520b7ffba4d9b4270092c159484e63e4debd265673d6aee84d'
+  config.secret_key = '5c81d0b42866aef97ed234489a52feb3f26cd810814f86c8c77a53bec5676fdf575edffa7351cc520b7ffba4d9b4270092c159484e63e4debd265673d6aee84d'
 
   # config.secret_key = ENV['DEVISE_SECRET_KEY']
 
